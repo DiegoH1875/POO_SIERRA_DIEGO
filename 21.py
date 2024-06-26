6e60e37552dbc89fe8579c72b0aed76e9a37afc0
Diego <diego.sierra.alcala@gmail.com>
2024-06-26 11:29:14 -0600

    Programa 21 Tupla desempaquetada

---
+++ 21.py
@@ -0,0 +1,4 @@
+persona = ("Francisco", "Perez", "Figueroa", "21","TI BIS")
+nombre, primer_apellido, segundo_apellido, edad, carrera = persona
+print(f"Nombre: {nombre}, Primer Apellido: {primer_apellido}, Segundo Apellido: {segundo_apellido}"
+      f"Edad: {edad}, Carrera: {carrera}")
